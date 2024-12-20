3696ced434b021aa5325912e39da1b6163659c1a
kreet1010 <kreetrout@gmail.com>
2024-12-20 14:17:17 +0530

    base structure

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import dr_samal from './assets/dr.sailesh_kumar_samal.jpg';
+import './App.css';
+import Navbar from './components/navbar.js';
+const ParallaxScrollEffect = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`container ${scrolled ? 'scrolled' : ''}`}>
+      <div className="white-div">
+        <div className="white-div-content">
+          <h1>Dr. Sailesh Kumar Samal</h1>
+          <h3>Co-Founder & CEO, Inflanova AB</h3>
+        </div>
+      </div>
+      <div className="black-div">
+        <div classname="black-div-content">
+          <img src={dr_samal} alt="Dr.Sailesh Kumar Samal"></img>
+        </div>
+      </div>
+      <Navbar />
+      <div className={`content-div ${scrolled ? 'visible' : ''}`}>
+        
+          <h1>About</h1>
+          <p>Dr. Shailesh Kumar Samal is a leading authority in immunotherapy and biotechnology, with dual Ph.D. degrees in Medical Sciences from Karolinska Institutet and Biochemistry from Maastricht University. As a serial entrepreneur, he has founded and led several biotech companies, including Inflanova AB. Dr. Samal has collaborated with Nobel laureates worldwide and is a nominated member of Sigma Xi, The Scientific Research Honor Society, known for its prestigious membership that includes 200 Nobel laureates. Sigma Xi, founded in 1886, honors excellence in scientific research and fosters collaboration among scientists and engineers. Dr. Samal also serves as an adjunct professor at IIT Ropar, where he is the youngest professor across all IITs. His pioneering work in developing vaccines and diagnostics for chronic inflammatory conditions reflects his deep commitment to advancing science and education.</p>
+        </div>
+        
+    </div>
+  );
+};
+
+export default ParallaxScrollEffect;
