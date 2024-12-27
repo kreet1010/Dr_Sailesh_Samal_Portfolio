a34111178d4dd73bbc494095dc91e041a116ff1a
kreet1010 <kreetrout@gmail.com>
2024-12-27 12:01:30 +0530

    colour scheme update & filter

--- src/ParalaxScrollEffect.js
+++ src/ParalaxScrollEffect.js
@@ -1,81 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import dr_samal from "./assets/dr.sailesh_kumar_samal.jpg";
 import Navbar from "./components/navbar.js";
 import analysisRprt from "./assets/crop.jpg";
 import "./App.css";
 
 const ParallaxScrollEffect = () => {
-  const [scrolled, setScrolled] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const handleClick = () => {
-    navigate("/journey"); // Navigate to the '/journey' route
-  };
 
   return (
-    <div className={`container ${scrolled ? "scrolled" : ""}`}>
-      <div className="white-div">
-        <div className="white-div-content">
-          <div className="welcome">Welcome</div>
-          <div className="name-txt">Dr. Sailesh Kumar Samal</div>
-          <div className="desigs">
+    <div className="landing">
+      <Navbar />
+      <div className="parallax1">
+        <div className="filter">
+          <div className="intro">
+            <h1>Dr. Sailesh Kumar Samal</h1>
             <h3>Co-Founder & CEO, Inflanova AB</h3>
             <h3>Senior Researcher Karolinska Institutet</h3>
             <h3>Adjunct fac./Prof - IIT Ropar</h3>
           </div>
         </div>
-        <div className="scroll_txt">Scroll</div>
       </div>
-      <div className="black-div">
-        <div className="black-div-content">
-          <img src={dr_samal} alt="Dr.Sailesh Kumar Samal" />
-        </div>
-      </div>
-      <div className={`behind-the-scene ${scrolled ? "visible" : ""}`}>
-        <Navbar />
-        <div className="layer-top">
-          <div className="about">
-            <h1>About</h1>
-            <p>
-            Dr. Shailesh Kumar Samal is a leading authority in immunotherapy and biotechnology, with dual Ph.D. degrees in Medical Sciences from Karolinska Institutet and Biochemistry from Maastricht University. As a serial entrepreneur, he has founded and led several biotech companies, including Inflanova AB. Dr. Samal has collaborated with Nobel laureates worldwide and is a nominated member of Sigma Xi, The Scientific Research Honor Society, known for its prestigious membership that includes 200 Nobel laureates. Sigma Xi, founded in 1886, honors excellence in scientific research and fosters collaboration among scientists and engineers. Dr. Samal also serves as an adjunct professor at IIT Ropar, where he is the youngest professor across all IITs. His pioneering work in developing vaccines and diagnostics for chronic inflammatory conditions reflects his deep commitment to advancing science and education.
-            </p>
-          </div>
-          <div className="image-container">
-            <img src={dr_samal} alt="Dr. Sailesh Kumar Samal" />
-          </div>
-        </div>
-        <div className="layer-btm">
-          <div className="card1">
-          <div className="total-citations"> 47 </div>
-            <div className="card-desc">Published Researches</div>
-          </div>
-          <div className="card2">
-            <div className="total-citations"> 406 </div>
-            <div className="card-desc">Total citations</div>
-          </div>
-          <div className="card3">
-            <div className="work">
-            Every great achievement starts with a dream. From humble beginnings to breaking barriers, this journey has been fueled by perseverance, passion, and an unwavering belief in the power of possibilities. Through countless challenges and lessons learned, each step forward has been a testament to resilience and growth.
-            </div>
-            <div className="journey-button" onClick={handleClick}>
-              View Journey
-            </div>
-          </div>
+      <div className="content">
+        <div className="text">
+          <h1>The Text</h1>
+          <p>
+            In the heart of a bustling city, there lies a hidden gem—a small, unassuming café that has become a sanctuary for those seeking solace from the chaos of urban life. The aroma of freshly brewed coffee mingles with the soft hum of conversation, creating an atmosphere that is both comforting and invigorating. The walls are adorned with local artwork, each piece telling a story of its own, while the shelves are lined with books waiting to be discovered by curious minds.
+            The café's owner, an elderly gentleman with a twinkle in his eye, greets each patron with a warm smile and a friendly word. He knows most of his customers by name, and their preferences are etched in his memory. Regulars come not just for the coffee, but for the sense of community that the café fosters. Strangers become friends over shared tables, and the worries of the outside world seem to melt away with each sip of the rich, dark brew.
+
+            As the sun sets and the city lights begin to twinkle, the café transforms into a cozy haven, where dreams are nurtured, and stories are shared. It is a place where time slows down, and the simple pleasures of life are savored.
+          </p>
         </div>
       </div>
     </div>
--- src/pages/news/news.js
+++ src/pages/news/news.js
@@ -1,70 +1,140 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import NewsCard from "../../components/newsCard/newsCard";
-import project1Image from "../../assets/disease.jpeg";
-import project2Image from "../../assets/medicine.jpeg";
 import Navbar from "../../components/navbar";
 import "./news.css";
 
 const News = () => {
   const [searchTerm, setSearchTerm] = useState("");
-
+  const [sortOption, setSortOption] = useState("");
   const projectData = [
     {
-      title: "ABC",
-      description: "A veryyyyyyyyy longgggggggggg descriptionnnnnnnnn.",
-      image: project1Image,
-      githubLink: "https://github.com/username/portfolio-website",
+      title: "Project A",
+      description: "This is a description for Project A.",
+      location: "New York",
+      date: "2024-01-01",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-a",
     },
     {
-      title: "DEF",
-      description: "Another very long desc.",
-      image: project2Image,
-      githubLink: "https://github.com/username/ecommerce-app",
+      title: "Project B",
+      description: "This is a description for Project B.",
+      location: "Los Angeles",
+      date: "2024-02-15",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-b",
     },
     {
-      title: "DEF",
-      description: "Another very long desc.",
-      image: project2Image,
-      githubLink: "https://github.com/username/ecommerce-app",
+      title: "Project C",
+      description: "This is a description for Project B.",
+      location: "Los Angeles",
+      date: "2024-02-15",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-b",
     },
     {
-      title: "DEF",
-      description: "Another very long desc.",
-      image: project2Image,
-      githubLink: "https://github.com/username/ecommerce-app",
+      title: "Project D",
+      description: "This is a description for Project B.",
+      location: "Los Angeles",
+      date: "2024-02-15",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-b",
     },
     {
-      title: "DEF",
-      description: "Another very long desc.",
-      image: project2Image,
-      githubLink: "https://github.com/username/ecommerce-app",
+      title: "Project E",
+      description: "This is a description for Project B.",
+      location: "Los Angeles",
+      date: "2024-02-15",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-b",
     },
     {
-      title: "ABC",
-      description: "A veryyyyyyyyy longgggggggggg descriptionnnnnnnnn.",
-      image: project1Image,
-      githubLink: "https://github.com/username/portfolio-website",
+      title: "Project F",
+      description: "This is a description for Project B.",
+      location: "Los Angeles",
+      date: "2024-02-15",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-b",
+    },
+    {
+      title: "Project G",
+      description: "This is a description for Project B.",
+      location: "Los Angeles",
+      date: "2024-02-15",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-b",
+    },
+    {
+      title: "Project H",
+      description: "This is a description for Project B.",
+      location: "Los Angeles",
+      date: "2024-02-15",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-b",
+    },
+    {
+      title: "Project I",
+      description: "This is a description for Project B.",
+      location: "Los Angeles",
+      date: "2024-02-15",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-b",
+    },
+    {
+      title: "Project J",
+      description: "This is a description for Project B.",
+      location: "Los Angeles",
+      date: "2024-02-15",
+      image: "https://via.placeholder.com/150",
+      githubLink: "https://github.com/username/project-b",
     },
   ];
+  const filteredProjects = projectData
+    .filter((project) =>
+      project.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (sortOption === "title") {
+        return a.title.localeCompare(b.title);
+      } else if (sortOption === "date") {
+        return new Date(a.date) - new Date(b.date);
+      } else if (sortOption === "location") {
+        return a.location.localeCompare(b.location);
+      }
+      return 0;
+    });
 
-  const filteredProjects = projectData.filter((project) =>
-    project.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
   return (
 
     <div className="projects-container">
       <Navbar />
-      <h1>My Projects</h1>
+      <h1>Articles & Updates</h1>
       <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search article by title..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search article by title..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+        <div className="sort-by">
+          <div className="sort-by">
+            <label htmlFor="sort">Sort By:</label>
+            <select
+              id="sort"
+              value={sortOption}
+              onChange={(e) => setSortOption(e.target.value)}
+            >
+              <option value="">None</option>
+              <option value="title">Title</option>
+              <option value="date">Date</option>
+              <option value="location">Location</option>
+            </select>
+          </div>
+        </div>
       </div>
       <div className="news-cards">
-      {filteredProjects.map((project, index) => (
+        {filteredProjects.map((project, index) => (
           <NewsCard
             key={index}
             title={project.title}
