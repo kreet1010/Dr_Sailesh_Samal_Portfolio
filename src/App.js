75267c82b3ddc21a1ffaf1e07512a750de618a4f
kreet1010 <kreetrout@gmail.com>
2025-01-07 21:35:27 +0530

    Routing Fixed

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import News from "./pages/news/news";
 import Error404 from "./pages/Error/Error";
 
 const App = () => {
+ 
   return (
     <Router>
       <Routes>
-        <Route path="/Dr_Sailesh_Samal_Portfolio" element={<ParallaxScrollEffect />} />
-        <Route path="/Dr_Sailesh_Samal_Portfolio/updates" element={<News />} />
-        <Route path="/Dr_Sailesh_Samal_Portfolio/journey" element={<Skills />} />
-        <Route path="/Dr_Sailesh_Samal_Portfolio/project" element={<Projects />} />
-        <Route path="/Dr_Sailesh_Samal_Portfolio/contact" element={<Contact />} />
-        <Route path="*" element={<Navigate to="/Dr_Sailesh_Samal_Portfolio" replace/>} />
-        <Route path="*" element={<Error404 />} />
+        <Route path="/Dr_Sailesh_Samal_Portfolio/" element={<ParallaxScrollEffect />} />
+        <Route path="/updates" element={<News />} />
+        <Route path="/journey" element={<Skills />} />
+        <Route path="/project" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Navigate to="/" replace/>} />
       </Routes>
     </Router>
   );
