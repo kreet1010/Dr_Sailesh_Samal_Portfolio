d59c650e881af00002e4d1ddc24f743c66a46af9
kreet1010 <kreetrout@gmail.com>
2025-01-18 01:11:10 +0530

    update before hashrouter

---
+++ src/pages/Blog/blog.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./blog.css";
+import Navbar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
+
+const Contact = () => {
+  
+
+  return (
+    <div className="blog-container">
+        <Navbar />
+        <Footer />
+    </div>
+  );
+};
+
+export default Contact;
